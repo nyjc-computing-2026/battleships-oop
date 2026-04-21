424f541750c3cebb4964ea0c562d600c980458ca
JS Ng <ngjunsiang@gmail.com>
2026-04-21 07:27:18 +0000

    end polymorphism

--- grid.py
+++ grid.py
@@ -12,8 +12,8 @@ import ship
 import utility
 
 
-class Grid:
-    """Represents a grid in Battleships.
+class AttackGrid:
+    """Represents an attack grid in Battleships.
 
     Each grid is a nested list (list of lists) where each inner list
     represents a row of the grid.
@@ -80,148 +80,182 @@ class Grid:
         return True
 
 
-def create_grid(n: int, placeholder: str) -> Grid:
-    """Create a n-by-n grid.
-    The grid is represented as a list of lists.
-    Each inner list represents a row.
-    Each row is filled with the placeholder character.
+class ShipGrid:
+    """Represents a ship grid in Battleships.
 
-    Arguments:
-        n: int
-            the size of the grid (n x n)
-        placeholder: str
-            the character to fill the grid with
-
-    Returns:
-        A nested list representing the grid.
+    Each grid is a nested list (list of lists) where each inner list
+    represents a row of the grid.
     """
-    return Grid(n, placeholder)
 
+    def __init__(self, size: int, placeholder: str) -> None:
+        self.size = size
+        self._data = []
+        for _ in range(size):
+            row = [placeholder] * size
+            self._data.append(row)
 
-def get_grid_coordinate_char(grid: Grid, x: int, y: int) -> str:
-    """Get the character at the specified grid coordinates.
+    def get(self, x: int, y: int) -> str:
+        """Get the character at the specified grid coordinates.
 
-    Arguments:
-        grid: Grid
-        x: int
-            the horizontal coordinate
-        y: int
-            the vertical coordinate
+        Arguments:
+            x: int
+                the horizontal coordinate
+            y: int
+                the vertical coordinate
     
-    Returns:
-        The character at the specified coordinates on the grid.
-    """
-    return grid.get(x, y)
+        Returns:
+            The character at the specified coordinates on the grid.
+        """
+        return self._data[y][x]
 
+    def set(self, x: int, y: int, symbol: str) -> None:
+        """Set the character at the specified grid coordinates.
 
-def initialize_grid(grid: Grid, ships: list[ship.Ship]) -> None:
-    """Initialize the grid by placing ships randomly on the grid.
-
-    Arguments:
-        grid: Grid
-            the game grid to initialize
-        ships: list[dict]
-            a list of ships to place on the grid
+        Arguments:
+            x: int
+                the horizontal coordinate
+            y: int
+                the vertical coordinate
+            symbol: str
+                the character to set at the specified coordinates
     
-    Returns:
-        None
-    """
-    for ship in ships:
-        x, y = utility.generate_random_coordinate(grid.size)
-        orientation = utility.generate_random_orientation()
-        # Keep trying to place the ship until it is successfully placed
-        # on the grid
-        while not place_ship_on_grid(grid, ship, x, y, orientation):
-            x, y = utility.generate_random_coordinate(grid.size)
+        Returns:
+            None
+        """
+        if not symbol.upper():
+            raise ValueError
+        self._data[y][x] = symbol
+
+    def display(self) -> None:
+        """Display the grid in a readable format."""
+        for row in self._data:
+            print(" ".join(row))
+
+    def is_valid_coordinate(self, x: int, y: int) -> bool:
+        """Check if the given coordinates are valid for the grid.
+
+        Arguments:
+            x: int
+                the row index to validate
+            y: int
+                the column index to validate
+
+        Returns:
+            True if the coordinates are valid, False otherwise.
+        """
+        if x < 0 or x >= self.size or y < 0 or y >= self.size:
+            return False
+        return True
+
+    def initialize_grid(self, ships: list[ship.Ship]) -> None:
+        """Initialize the grid by placing ships randomly on the grid.
+
+        Arguments:
+            grid: Grid
+                the game grid to initialize
+            ships: list[dict]
+                a list of ships to place on the grid
+        
+        Returns:
+            None
+        """
+        for ship in ships:
+            x, y = utility.generate_random_coordinate(self.size)
             orientation = utility.generate_random_orientation()
+            # Keep trying to place the ship until it is successfully placed
+            # on the grid
+            while not self.place_ship_on_grid(ship, x, y, orientation):
+                x, y = utility.generate_random_coordinate(self.size)
+                orientation = utility.generate_random_orientation()
 
 
-def place_ship_on_grid(
-        grid: Grid,
-        ship: ship.Ship,
-        x: int,
-        y: int,
-        orientation: str
-) -> bool:
-    """Place a ship on the grid at the specified coordinates.
+    def place_ship_on_grid(
+            self,
+            ship: ship.Ship,
+            x: int,
+            y: int,
+            orientation: str
+    ) -> bool:
+        """Place a ship on the grid at the specified coordinates.
 
-    Arguments:
-        grid: list[list[str]] -- the game grid
-        ship: dict -- the ship to place on the grid
-        x: int -- the row index for the starting coordinate
-        y: int -- the column index for the starting coordinate
-        orientation: str -- the orientation of the ship ('horizontal' or
-            'vertical')
-    
-    Returns:
-        True if the ship was successfully placed, False if placement
-        failed due to out-of-bounds or overlap with existing ships.
-    """
-    if orientation == 'horizontal':
-        return place_ship_on_grid_horizontally(grid, ship, x, y)
-    elif orientation == 'vertical':
-        return place_ship_on_grid_vertically(grid, ship, x, y)
-    else:
-        raise ValueError(
-            "Invalid orientation. Must be 'horizontal' or 'vertical'."
-        )
-
-
-def place_ship_on_grid_horizontally(
-        grid: Grid,
-        ship: ship.Ship,
-        x: int,
-        y: int,
-) -> bool:
-    """Place a ship on the grid at the specified coordinates
-    horizontally (rightwards).
-
-    Arguments:
-        grid: Grid -- the game grid
-        ship: Ship -- the ship to place on the grid
-        x: int -- the row index for the starting coordinate
-        y: int -- the column index for the starting coordinate
-    
-    Returns:
-        True if the ship was successfully placed, False if placement
-        failed due to out-of-bounds or overlap with existing ships.
-    """
-    for i in range(ship.length):
-        if not grid.is_valid_coordinate(x, y + i) or grid.get(x, y + i) != '~':
-            return False
-    for i in range(ship.length):
-        grid.set(x, y + i, ship.symbol)
-    return True
+        Arguments:
+            grid: list[list[str]] -- the game grid
+            ship: dict -- the ship to place on the grid
+            x: int -- the row index for the starting coordinate
+            y: int -- the column index for the starting coordinate
+            orientation: str -- the orientation of the ship ('horizontal' or
+                'vertical')
+        
+        Returns:
+            True if the ship was successfully placed, False if placement
+            failed due to out-of-bounds or overlap with existing ships.
+        """
+        if orientation == 'horizontal':
+            return self.place_ship_on_grid_horizontally(ship, x, y)
+        elif orientation == 'vertical':
+            return self.place_ship_on_grid_vertically(ship, x, y)
+        else:
+            raise ValueError(
+                "Invalid orientation. Must be 'horizontal' or 'vertical'."
+            )
 
 
-def place_ship_on_grid_vertically(
-        grid: Grid,
-        ship: ship.Ship,
-        x: int,
-        y: int,
-) -> bool:
-    """Place a ship on the grid at the specified coordinates vertically
-    (downwards).
+    def place_ship_on_grid_horizontally(
+            self,
+            ship: ship.Ship,
+            x: int,
+            y: int,
+    ) -> bool:
+        """Place a ship on the grid at the specified coordinates
+        horizontally (rightwards).
 
-    Arguments:
-        grid: list[list[str]] -- the game grid
-        ship: Ship -- the ship to place on the grid
-        x: int -- the row index for the starting coordinate
-        y: int -- the column index for the starting coordinate
-    
-    Returns:
-        True if the ship was successfully placed, False if placement
-        failed due to out-of-bounds or overlap with existing ships.
-    """
-    for i in range(ship.length):
-        if (
-                not grid.is_valid_coordinate(x + i, y)
-                or grid.get(x + i, y) != '~'
-        ):
-            return False
-    for i in range(ship.length):
-        grid.set(x + i, y, ship.symbol)
-    return True
+        Arguments:
+            grid: Grid -- the game grid
+            ship: Ship -- the ship to place on the grid
+            x: int -- the row index for the starting coordinate
+            y: int -- the column index for the starting coordinate
+        
+        Returns:
+            True if the ship was successfully placed, False if placement
+            failed due to out-of-bounds or overlap with existing ships.
+        """
+        for i in range(ship.length):
+            if not self.is_valid_coordinate(x, y + i) or self.get(x, y + i) != '~':
+                return False
+        for i in range(ship.length):
+            self.set(x, y + i, ship.symbol)
+        return True
+
+
+    def place_ship_on_grid_vertically(
+            self,
+            ship: ship.Ship,
+            x: int,
+            y: int,
+    ) -> bool:
+        """Place a ship on the grid at the specified coordinates vertically
+        (downwards).
+
+        Arguments:
+            grid: list[list[str]] -- the game grid
+            ship: Ship -- the ship to place on the grid
+            x: int -- the row index for the starting coordinate
+            y: int -- the column index for the starting coordinate
+        
+        Returns:
+            True if the ship was successfully placed, False if placement
+            failed due to out-of-bounds or overlap with existing ships.
+        """
+        for i in range(ship.length):
+            if (
+                    not self.is_valid_coordinate(x + i, y)
+                    or self.get(x + i, y) != '~'
+            ):
+                return False
+        for i in range(ship.length):
+            self.set(x + i, y, ship.symbol)
+        return True
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -6,9 +6,10 @@ and the number of turns taken.
 
 import grid
 import ship
+import utility
 
 
-class Player:
+class Human:
     """Represents a player in Battleships.
 
     Each player records information about:
@@ -20,8 +21,8 @@ class Player:
     def __init__(
             self,
             name: str,
-            ship_board: grid.Grid,
-            attack_board: grid.Grid,
+            ship_board: grid.ShipGrid,
+            attack_board: grid.AttackGrid,
             ships: dict[str, ship.Ship],
     ) -> None:
         self.name = name
@@ -29,6 +30,18 @@ class Player:
         self.attack_board = attack_board
         self.ships = ships
         self.turns_taken = 0
+
+    def get_input(self) -> tuple[int, int]:
+        """Get input coordinates for human's attack."""
+        userinput = input("Enter x and y coordinates (e.g. '3 4'): ")
+        while not is_input_valid(userinput, self.attack_board.size):
+            print(
+                "Invalid input. Please enter x and y coordinates as two "
+                "integers separated by a space."
+            )
+            userinput = input("Enter x and y coordinates (e.g. '3 4'): ")
+        row_str, col_str = userinput.split()
+        return int(row_str), int(col_str)
 
     def has_player_lost(self, max_turns: int) -> bool:
         """Check if the player has lost the game.
@@ -49,6 +62,17 @@ class Player:
             if not player_ship.is_sunk():
                 return False
         return True
+
+    def take_turn(self) -> None:
+        """Increment the number of turns taken by the player.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
+        self.turns_taken += 1
 
     def update_attack(
             self,
@@ -106,44 +130,118 @@ class Player:
                 self.ships[symbol].hit()
 
 
-def create_player(
-        name: str,
-        ship_board: grid.Grid,  # grid
-        attack_board: grid.Grid,  # grid
-        ships: dict[str, ship.Ship],  # symbol: ship
-) -> Player:
-    """Create a player with the given name and an empty grid.
+class Computer:
+    """Represents a player in Battleships.
 
-    Arguments:
-        name: str
-            the name of the player
-        ship_board: grid.Grid
-            the player's ship board
-        attack_board: grid.Grid
-            the player's attack board
-        ships: dict[str, Ship]
-            a dictionary mapping ship symbols to their details
-
-    Returns:
-        A dictionary representing the player, containing their name and
-        grid.
-        Format:
-        {
-            'name': str,
-            'ship_board': list[list[str]],
-            'attack_board': list[list[str]],
-            'ships': {
-                "B": ...
-                "C": ...
-                "D": ...
-            },
-            'turns_taken': int,
-        }
+    Each player records information about:
+    - player's name
+    - the player's ship board (containing ship positions)
+    - the player's attack board (containing attack guesses and results)
+    - the player's ship status (number hits) 
     """
-    player = Player(name, ship_board, attack_board, ships)
-    return player
-    p1 = Player(name, ship_board, attack_board, ships)
-    return p1
+
+    def __init__(
+            self,
+            name: str,
+            ship_board: grid.ShipGrid,
+            attack_board: grid.AttackGrid,
+            ships: dict[str, ship.Ship],
+    ) -> None:
+        self.name = name
+        self.ship_board = ship_board
+        self.attack_board = attack_board
+        self.ships = ships
+        self.turns_taken = 0
+
+    def get_input(self) -> tuple[int, int]:
+        """Get input coordinates for computer's attack."""
+        return utility.generate_random_coordinate(self.attack_board.size)
+
+    def has_player_lost(self, max_turns: int) -> bool:
+        """Check if the player has lost the game.
+        A player loses when:
+        - all of their ships have been sunk.
+        - they have exceeded the maximum number of turns.
+
+        Arguments:
+            max_turns: int
+                the maximum number of turns allowed for the player
+
+        Returns:
+            True if the player has lost, False otherwise.
+        """
+        if self.turns_taken >= max_turns:
+            return True
+        for player_ship in self.ships.values():
+            if not player_ship.is_sunk():
+                return False
+        return True
+
+    def take_turn(self) -> None:
+        """Increment the number of turns taken by the player.
+
+        Arguments:
+            None
+
+        Returns:
+            None
+        """
+        self.turns_taken += 1
+
+    def update_attack(
+            self,
+            x: int,
+            y: int,
+            symbol: str
+    ) -> None:
+        """Update the attacker's attack board based on the result of an
+        attack.
+
+        Arguments:
+            attacker: Player
+                the player whose attack board is to be updated
+            x: int
+                the horizontal coordinate of the attack
+            y: int
+                the vertical coordinate of the attack
+            symbol: str
+                the symbol to represent the attack result on the board
+
+        Returns:
+            None
+        """
+        self.attack_board.set(x, y, symbol)
+
+    def update_defense(
+            self,
+            x: int,
+            y: int,
+            symbol: str
+    ) -> None:
+        """Update the defender's ship board based on the result of an attack.
+
+        Arguments:
+            defender: Player
+                the player whose ship board is to be updated
+            x: int
+                the horizontal coordinate of the attack
+            y: int
+                the vertical coordinate of the attack
+            symbol: str
+                the symbol to represent the attack result on the board
+
+        Returns:
+            None
+        """
+        if symbol == "~":
+            self.ship_board.set(x, y, "O")  # Miss
+        else:
+            self.ship_board.set(x, y, "X")  # Hit
+            if (
+                    symbol in self.ships
+                    and not self.ships[symbol].is_sunk()
+            ):
+                self.ships[symbol].hit()
 
 
 def get_player_input(size: int) -> tuple[int, int]:
