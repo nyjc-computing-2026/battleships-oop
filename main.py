37b9ab2367908fc4b28709946db70e5bfa9193a9
JS Ng <ngjunsiang@gmail.com>
2026-04-24 04:11:20 +0000

    refactor: encapsulate turns_taken through take_turn() method in Player class

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main() -> None:
             human.update_attack(x, y, "O")
             human.update_defense(x, y, "O")
         print(message)
-        human.turns_taken += 1
+        human.take_turn()
 
         # Computer's turn
         print(f"{computer.name}'s turn:")
@@ -87,7 +87,7 @@ def main() -> None:
         # Show player's ship board after input
         computer.ship_board.display()
         print(message)
-        computer.turns_taken += 1
+        computer.take_turn()
 
 
 if __name__ == "__main__":
